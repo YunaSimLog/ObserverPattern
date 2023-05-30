be7e38891aa5fc8ca67af5514b176c61cf8fc054
yunasim <ynsim@solidit.co.kr>
2023-05-31 00:48:14 +0900

    #✨feature: 테스트용 Form 작성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.lbNote = new System.Windows.Forms.Label();
+            this.pnlColor = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUpdate.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnUpdate.Location = new System.Drawing.Point(12, 202);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(113, 31);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "업데이트";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(133, 202);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(96, 31);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNote.Location = new System.Drawing.Point(12, 35);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(215, 21);
+            this.tbNote.TabIndex = 2;
+            // 
+            // lbNote
+            // 
+            this.lbNote.AutoSize = true;
+            this.lbNote.Location = new System.Drawing.Point(12, 20);
+            this.lbNote.Name = "lbNote";
+            this.lbNote.Size = new System.Drawing.Size(57, 12);
+            this.lbNote.TabIndex = 3;
+            this.lbNote.Text = "문자 입력";
+            // 
+            // pnlColor
+            // 
+            this.pnlColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlColor.BackColor = System.Drawing.Color.Linen;
+            this.pnlColor.Location = new System.Drawing.Point(14, 111);
+            this.pnlColor.Name = "pnlColor";
+            this.pnlColor.Size = new System.Drawing.Size(213, 29);
+            this.pnlColor.TabIndex = 4;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Font = new System.Drawing.Font("굴림", 9F);
+            this.btnColor.Location = new System.Drawing.Point(12, 74);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(111, 31);
+            this.btnColor.TabIndex = 5;
+            this.btnColor.Text = "색상 편집";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("굴림", 9F);
+            this.button1.Location = new System.Drawing.Point(14, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Form2 보이기";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("굴림", 9F);
+            this.button2.Location = new System.Drawing.Point(131, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Form3 보이기";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 251);
+            this.ClientSize = new System.Drawing.Size(241, 245);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.pnlColor);
+            this.Controls.Add(this.lbNote);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnUpdate);
+            this.MinimumSize = new System.Drawing.Size(257, 284);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.Label lbNote;
+        private System.Windows.Forms.Panel pnlColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
